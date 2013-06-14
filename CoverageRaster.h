b6ee393a9e07e766e9abd523946d04fb5771febf
Aras Pranckevicius <aras@unity3d.com>
2013-06-14 21:54:16 +0300

    comments

--- CoverageRaster.h
+++ CoverageRaster.h
@@ -1,5 +1,8 @@
 #pragma once
 
+// Rasterize 2D triangles into 1 byte-per-pixel image.
+// @param indices Index buffer, 3 ints per triangle.
+// @param vertices Vertices, 2 floats per vertex; 0..1 range over the image.
 void RasterizeCoverage(
 					   int triCount,
 					   const int* indices,
